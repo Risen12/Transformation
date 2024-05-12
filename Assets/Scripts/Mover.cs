dbdaf895d7046a08db09ddb3cb189079c44a7453
Risen12 <den2033@bk.ru>
2024-05-12 16:41:40 +0300

    Four iteration

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,34 +2,12 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private Vector3 _currentPosition;
+    [SerializeField] private Vector3 _direction;
 
-    private int _maxPosition;
-    private int _minPosition;
-    private bool _isNeedToReverse;
-
-    private void Start()
-    {
-        _maxPosition = 6;
-        _minPosition = 2;
-        _isNeedToReverse = false;
-    }
+    private const string Vertical = nameof(Vertical); 
 
     private void Update()
     {
-        if(_isNeedToReverse)
-        transform.Translate(-_currentPosition);
-        else
-            transform.Translate(_currentPosition);
-
-        VerifyDirection();
-    }
-
-    private void VerifyDirection()
-    { 
-        if(transform.position.x >= _maxPosition)
-            _isNeedToReverse = true;
-        else if(transform.position.x <= _minPosition)
-            _isNeedToReverse = false;
+        transform.Translate(_direction);
     }
 }
--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -4,45 +4,8 @@ public class Scaler : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    protected int _maxSize;
-    protected int _minSize;
-    protected bool _isNeedToGrow;
-
-    private void Start()
-    {
-        _maxSize = 2;
-        _minSize = 1;
-        _isNeedToGrow = true;
-    }
-
     private void Update()
     {
-        if (_isNeedToGrow)
-            Grow();
-        else
-            Decrease();
-
-        VerifyScale();
-    }
-
-    protected void Grow()
-    {
-        Vector3 scale = new Vector3(_speed * Time.deltaTime, _speed * Time.deltaTime, _speed * Time.deltaTime);
-        transform.localScale += scale;
-    }
-
-    protected void Decrease()
-    {
-        Vector3 scale = new Vector3(_speed * Time.deltaTime, _speed * Time.deltaTime, _speed * Time.deltaTime);
-        transform.localScale -= scale;
-    }
-
-    protected void VerifyScale()
-    {
-        if (transform.localScale.x <= _minSize && transform.localScale.y <= _minSize && transform.localScale.z <= _minSize)
-            _isNeedToGrow = true;
-        else if(transform.localScale.x > _maxSize && transform.localScale.y > _maxSize && transform.localScale.z > _maxSize)
-            _isNeedToGrow = false;
-
+        transform.localScale += new Vector3(_speed * Time.deltaTime, _speed * Time.deltaTime, _speed * Time.deltaTime);
     }
 }
